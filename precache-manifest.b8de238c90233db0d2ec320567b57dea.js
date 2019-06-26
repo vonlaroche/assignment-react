38adbeec097eb1ddc021538dcbaf546b088127db
vonlaroche <dominic.monika@gmail.com>
2019-06-26 11:37:01 +0200

    Updates

---
+++ precache-manifest.b8de238c90233db0d2ec320567b57dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a136034c186679b3024b19c7fb7d534d",
+    "url": "/vonlaroche/assignment-react/index.html"
+  },
+  {
+    "revision": "ec1c8bb9abf10a455176",
+    "url": "/vonlaroche/assignment-react/static/css/main.a9291b9a.chunk.css"
+  },
+  {
+    "revision": "71594d41ec249c6d5e03",
+    "url": "/vonlaroche/assignment-react/static/js/2.6e498eb3.chunk.js"
+  },
+  {
+    "revision": "ec1c8bb9abf10a455176",
+    "url": "/vonlaroche/assignment-react/static/js/main.0ae1a142.chunk.js"
+  },
+  {
+    "revision": "45a66112a6779f83541b",
+    "url": "/vonlaroche/assignment-react/static/js/runtime~main.48654e49.js"
+  }
+]);
