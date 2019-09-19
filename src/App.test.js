b490084850130bbc7c30750bb0a683c1c045efea
unknown <dominic.monika@gmail.com>
2019-09-19 12:08:44 +0200

    removed useless tests

--- src/App.test.js
+++ src/App.test.js
@@ -10,39 +10,3 @@ const  notesToTest = {
   emptyNote: "", 
   tooLongNote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus ante metus, vel consectetur urna mollis ut. Fusce vel massa vestibulum, viverra sem eget, gravida lectus. Aliquam ac dui non.", 
   emojiNote: "😀😀😀"}
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-describe("Note List Item component", () => {
-  test("it matches the snapshot, proper note, no errors", () => {
-    const component = create(<NoteListItem checkNote={notesToTest.validNote}/>);
-    expect(component.toJSON()).toMatchSnapshot();
-  });
-});
-
-describe("Note List Item component", () => {
-  test("it matches the snapshot, error, empty note", () => {
-    const component = create(<NoteListItem checkNote={notesToTest.emptyNote}/>);
-    expect(component.toJSON()).toMatchSnapshot();
-  });
-});
-
-
-describe("Note List Item component", () => {
-  test("it matches the snapshot, error, longer than a 100", () => {
-    const component = create(<NoteListItem checkNote={notesToTest.tooLongNote}/>);
-    expect(component.toJSON()).toMatchSnapshot();
-  });
-});
-
-
-describe("Note List Item component", () => {
-  test("it matches the snapshot, error, contains emoji", () => {
-    const component = create(<NoteListItem checkNote={notesToTest.emojiNote}/>);
-    expect(component.toJSON()).toMatchSnapshot();
-  });
-});
